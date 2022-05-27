840774edc25a8e9175b97c59d5a42456a9148b46
Batuhan Özkoç <batur.ozkoc@gmail.com>
2022-05-27 23:13:26 +0300

    Edits for test purposes

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-      <CoinPrice coinSymbol="ETHTRY" />
+      <CoinPrice coinSymbol="ETHUSDT" />
 
-      <CoinPrice coinSymbol="BTCTRY" />
+      <CoinPrice coinSymbol="BTCUSDT" />
 
-      <CoinPrice coinSymbol="USDTTRY" />
+      <CoinPrice coinSymbol="LUNAUSDT" />
 
       <CoinPrice />
   </React.StrictMode>
